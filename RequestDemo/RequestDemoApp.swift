58fb3e9c633e64824a81545b3c1eb31a8cf9c7b6
QiZhao <362693354@qq.com>
2025-04-07 14:10:25 +0800

    Initial Commit

---
+++ RequestDemo/RequestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestDemoApp.swift
+//  RequestDemo
+//
+//  Created by ZQ on 2025/4/7.
+//
+
+import SwiftUI
+
+@main
+struct RequestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RequestDemoTests/RequestDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RequestDemoTests.swift
+//  RequestDemoTests
+//
+//  Created by ZQ on 2025/4/7.
+//
+
+import Testing
+@testable import RequestDemo
+
+struct RequestDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
